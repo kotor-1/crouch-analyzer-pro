dec06280736701e8bd849a4c427a1ef43385c41b
kotor-1 <ryohei2810@gmail.com>
2025-08-06 07:47:45 +0900

    Delete build_packages.sh

--- build_packages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Install Pillow from binary wheel to avoid compilation issues
-pip install Flask==3.0.0 gunicorn==21.2.0
-pip install --no-binary :all: --only-binary Pillow Pillow==9.5.0
